6d4fb25f363298ce232e66154fb713bbcc782f4a
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-06-27 10:18:14 +0000

    Start on packaging files.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="JSONLogging",
+    version="0.0.1",
+    packages=["jsonlogging"],
+    license="BSD",
+    long_description=open("README.txt").read()
+)
