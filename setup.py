5e320afd8ead9b626d1f85af1697c336a1b50368
hwtb2 <hwtb2@59899196-baf2-4bc4-a94d-4a08719e171f>
2013-07-12 16:23:25 +0000

    Pull __version__ from jsonlogging/__init__.py in setup.py.

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 from distutils.core import setup
 
+from jsonlogging import __version__
+
+def get_version(filename):
+    """
+    Parse the value of the __version__ var from a Python source file
+    without running/importing the file.
+    """
+    import re
+    version_pattern = r"^ *__version__ *= *['\"](\d+\.\d+\.\d+)['\"] *$"
+    match = re.search(version_pattern, open(filename).read(), re.MULTILINE)
+
+    assert match, ("No version found in file: {!r} matching pattern: {!r}"
+                   .format(filename, version_pattern))
+
+    return match.group(1)
+
 setup(
     name="jsonlogging",
-    version="0.0.2dev",
+    version=get_version("jsonlogging/__init__.py"),
     packages=["jsonlogging"],
     license="BSD",
     long_description=open("README.md").read()
